36aa0f257cb23d69ef847f0365506fc3570e0f87
antonioPompeo <pompeo.gabriel@outlook.com>
2022-01-10 06:07:07 -0300

    img update

--- root_page/models.py
+++ root_page/models.py
@@ -6,7 +6,7 @@ from django.db.models.fields.files import ImageField
 
 class Project(models.Model):
     title = CharField(max_length=40)
-    description = CharField(max_length=140)
+    description = CharField(max_length=200)
     image = ImageField(upload_to = "root_page/images/")  #lo va a guardar root_page/images pero en una carpeta llamada media porque es lo que setee en el archivo settings.py 
     url_github = URLField(blank = True)
     url_colab = URLField(blank = True)
